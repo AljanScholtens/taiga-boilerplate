8db53e04ee163d60062207fb0c0e3deedccfc994
Aljan Scholtens <aljan@studiowolf.nl>
2016-01-22 12:46:27 +0100

    Trying to get cssnext to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,41 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
 var lost = require('lost');
-var autoprefixer = require('gulp-autoprefixer');
+var cssnext = require('gulp-cssnext');
 
-gulp.task('sass', function () {
-  gulp.src('assets/stylesheets/styles.scss')
+// gulp.task('css', function () {
+//   gulp.src('assets/stylesheets/styles.scss')
+//
+//     .pipe(sass().on('error', sass.logError))
+//
+//     .pipe(sass({outputStyle: 'compressed'}))
+//
+//     .pipe(postcss([
+//       lost()
+//     ]))
+//
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions'],
+//       cascade: false,
+//     }))
+//
+//     .pipe(gulp.dest('dist/stylesheets'));
+// });
 
-    .pipe(sass().on('error', sass.logError))
-
-    .pipe(sass({outputStyle: 'compressed'}))
-
-    .pipe(postcss([
-      lost()
-    ]))
-
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }))
-
+gulp.task("css", function() {
+  var processors = [
+    cssnext,
+    lost()
+  ];
+  return gulp.src('assets/stylesheets/styles.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/stylesheets'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('assets/stylesheets/*.scss',['sass']);
+    gulp.watch('assets/stylesheets/*.css',['css']);
 });
