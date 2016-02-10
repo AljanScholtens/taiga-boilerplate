8a2379ea20f626703948c354b1e69f4431fc9654
Aljan Scholtens <aljan@studiowolf.nl>
2016-02-10 16:37:22 +0100

    making gulpfile smaller

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,35 @@
+var browserslist = require('browserslist');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
-// var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
-var browserslist = require('browserslist');
 var cssnano = require('gulp-cssnano');
 var atImport = require('postcss-import');
 var lost = require('lost');
 var cssnext = require('postcss-cssnext');
-var postcssExtend = require('postcss-simple-extend');
+
+// browsers for which autoprefix will add prefixes
+var browsers = 'last 2 versions';
 
 gulp.task('default',function() {
-  gulp.watch('assets/stylesheets/*.css',['css', 'patterns']);
+  gulp.watch('assets/stylesheets/*.css',['css']);
 });
 
+// Compile CSS
 gulp.task("css", function() {
   var processors = [
     atImport,
-    postcssExtend,
-    cssnext,
-    // cssnext([
-    //   browsers: 'last 2 versions',
-    //   features: {
-    //     rem: false
-    //   },
-    // ]),
+    cssnext(),
     lost()
-
-    // autoprefixer({ browsers: ['ie >= 11'] })
   ];
-  return gulp.src('assets/stylesheets/styles.css')
+  return gulp.src(['assets/stylesheets/styles.css', 'assets/stylesheets/pattern-library.css'])
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(cssnano())
     .pipe(sourcemaps.write('.'))
-
-    // IE7/8 CSS support is disbabled on default
-    // .pipe(autoprefixer({
-    //   browsers: ['last 2 versions'],
-    //   cascade: false,
-    // }))
-
     .pipe(gulp.dest('dist/stylesheets'));
 });
 
-gulp.task("patterns", function() {
-  var processors = [
-    atImport,
-    postcssExtend,
-    cssnext,
-    // cssnext([
-    //   browsers: 'last 2 versions',
-    //   features: {
-    //     rem: false
-    //   },
-    // ]),
-    lost()
-    // autoprefixer({ browsers: ['ie >= 11'] })
-  ];
-  return gulp.src('assets/stylesheets/pattern-library.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss(processors))
-    .pipe(cssnano())
-    .pipe(sourcemaps.write('.'))
-
-    // IE7/8 CSS support is disbabled on default
-    // .pipe(autoprefixer({
-    //   browsers: ['last 2 versions'],
-    //   cascade: false,
-    // }))
-
-    .pipe(gulp.dest('dist/stylesheets'));
+// display browsers
+gulp.task('browserslist', function(){
+  console.log(browserslist(browsers));
 });
