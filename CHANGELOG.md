71f90ea015649eae68d8d581c9f4fbe2b4d6b1b2
Aljan Scholtens <aljan@scholtens.me>
2016-12-15 16:43:10 +0100

    Building Taiga on nunjucks

--- CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-### January 21, 2016
-
-* Taiga is now in BEM.
-* Added the ITCSS mindset of Harry Roberts.
-* [CSS Guidelines](Guidelin.es) added.
-* PostCSS added
-* PostCSS Lost Grid added
-* Gulp added
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,82 @@
-var browserslist = require('browserslist');
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var postcss = require('gulp-postcss');
-var cssnano = require('gulp-cssnano');
-var atImport = require('postcss-import');
-var lost = require('lost');
-var cssnext = require('postcss-cssnext');
+var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
+var postcss = require('gulp-postcss')
+var cssnano = require('gulp-cssnano')
+var atImport = require('postcss-import')
+var lost = require('lost')
+var cssnext = require('postcss-cssnext')
+var nunjucks = require('gulp-nunjucks')
+var browserSync = require('browser-sync').create()
+var modRewrite  = require('connect-modrewrite')
+var smushit = require('gulp-smushit');
+var del = require('del');
 
-// browsers for which autoprefix will add prefixes
-var browsers = 'last 2 versions';
+gulp.task('default', ['clean', 'serve'])
 
-gulp.task('default',function() {
-  gulp.watch('assets/stylesheets/*.css',['css']);
+gulp.task('serve', ['build', 'watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/",
+      middleware: [
+        modRewrite([
+          '^/([^\.]+)$ /$1.html [L]'
+        ])
+      ]
+    },
+    open: false
+  });
+  gulp.watch('dist/**/*').on('change', browserSync.reload)
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['html'])
+  gulp.watch('src/assets/stylesheets/**/*.css', ['css'])
+  gulp.watch('src/assets/images/**/*', ['images'])
+  gulp.watch('src/assets/media/**/*', ['media'])
+  gulp.watch('src/assets/scripts/**/*', ['scripts'])
+})
+
+gulp.task('build', ['html', 'css', 'scripts', 'images', 'media'])
+
+gulp.task('clean', function(cb) {
+  // return del('dist');
 });
 
-// Compile CSS
+gulp.task('html', () =>
+  gulp.src('src/**/*.html')
+   .pipe(nunjucks.compile())
+   .pipe(gulp.dest('dist'))
+)
+
+gulp.task('images', () =>
+  gulp.src('src/assets/images/*')
+    .pipe(gulp.dest('dist/assets/images'))
+)
+
+gulp.task('media', () =>
+  gulp.src('src/assets/media/**/*')
+    // .pipe(smushit())
+    // .pipe(imagemin({
+    //     progressive: true,
+    // }))
+    .pipe(gulp.dest('dist/assets/media'))
+)
+
+gulp.task('scripts', () =>
+  gulp.src('src/assets/scripts/*')
+    .pipe(gulp.dest('dist/assets/scripts'))
+)
+
 gulp.task("css", function() {
   var processors = [
     atImport,
     cssnext(),
     lost()
-  ];
-  return gulp.src(['assets/stylesheets/styles.css', 'assets/stylesheets/styleguide.css'])
+  ]
+  gulp.src(['src/assets/stylesheets/styles.css', 'src/assets/stylesheets/styleguide.css'])
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(cssnano())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/stylesheets'));
-});
-
-// display browsers
-gulp.task('browserslist', function(){
-  console.log(browserslist(browsers));
-});
+    .pipe(gulp.dest('dist/assets/stylesheets'))
+})
